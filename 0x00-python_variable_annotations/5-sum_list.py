38fce8ab9c8103303ee10355a3a29791bc9c274c
Akinola <akinolatofunmi04@gmail.com>
2023-03-30 23:43:11 +0000

    Document

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -7,7 +7,7 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    """ """
+    """ Return the sum of floats in a list """
     sum = 0
     for num in input_list:
         sum += num
