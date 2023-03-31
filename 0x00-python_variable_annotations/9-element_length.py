74b2d3c5a07d980428b0ce9dc4c16e6fbd352fe1
Akinola <akinolatofunmi04@gmail.com>
2023-03-31 00:52:29 +0000

    Task 9 - Duck type an interable object

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Duck type an Iterable object
+"""
+
+
+from typing import List, Tuple, Iterator, Sequence
+
+
+def element_length(lst: Iterator[Sequence]) -> List[Tuple[Sequence, int]]:
+    """ Returns the value and length of value of the list """
+    return [(i, len(i)) for i in lst]
