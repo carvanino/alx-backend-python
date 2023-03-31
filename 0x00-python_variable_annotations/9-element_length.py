681568a3ff53f63402954b1826832e93f10bef0c
Akinola <akinolatofunmi04@gmail.com>
2023-03-31 01:04:53 +0000

    Fix import from iterator to iterable

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Duck type an Iterable object"""
-from typing import List, Tuple, Iterator, Sequence
+from typing import List, Tuple, Iterable, Sequence
 
 
-def element_length(lst: Iterator[Sequence]) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """ Returns the value and length of value of the list """
     return [(i, len(i)) for i in lst]
