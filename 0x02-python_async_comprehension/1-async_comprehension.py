57e6ed5b791ba43f274600048a51580ca9687c10
Akinola <akinolatofunmi04@gmail.com>
2023-04-04 09:53:01 +0000

    Task 1 -Async Comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 """
+Async Comprehension
 """
 
-import asycio
+import asyncio
+from typing import Generator
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Genrator[int, None, None]:
+async def async_comprehension() -> Generator[float, None, None]:
     """
-
+    Collects the yeilded value form async_generator and returns a
+    list of all the value
     """
+    rand_num = [rand_int async for rand_int in async_generator()]
+    return rand_num
+'''
+    lisN = []
+    async for rand_int in async_generator():
+        lisN.append(rand_int)
+    return lisN
+'''
