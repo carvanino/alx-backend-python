3d2b2462b4deba1d2a302266ad052b6e7dfcc786
Akinola <akinolatofunmi04@gmail.com>
2023-04-04 02:06:18 +0000

    Task 4 - Tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+
+"""
+
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawns task_wait_random coroutine n times with a delay of max_delay
+    """
+    task = [await (task_wait_random(max_delay)) for i in range(n)]
+    # result = await asyncio.gather(*task)
+    return sorted(task)
