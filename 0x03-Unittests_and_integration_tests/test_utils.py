8b66ff2fa9c4b99b6f84efd10b3ed2e46a656829
Akinola <akinolatofunmi04@gmail.com>
2023-05-20 14:11:33 +0000

    Documentation

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,15 +5,17 @@ Parameterize a unit test
 
 import unittest
 from parameterized import parameterized, parameterized_class
-from nose.tools import assert_equal
 from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """
+    Test access_nested_map utils methods
+    """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
         ])
-    def test_access_nested_map(self, a, b, expected):
-        assert_equal(access_nested_map(a, b), expected)
+    def test_access_nested_map(self, nested_map, path, expected):
+        self.assertEqual(access_nested_map(nested_map, path), expected)
