8daebb11441d58c8fe929665c07809a4983c034a
Akinola <akinolatofunmi04@gmail.com>
2023-05-21 15:40:52 +0000

    Task 6 - More Patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -7,6 +7,7 @@ import unittest
 from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 import utils
+import client
 
 from client import GithubOrgClient
 
@@ -41,3 +42,25 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_org.return_value = test_payload
             ghc = GithubOrgClient('org')
             self.assertEqual(ghc._public_repos_url, test_payload["repos_url"])
+
+    @patch("client.get_json")
+    def test_public_repos(self, mock_get_json):
+        """
+        Test public_repos from client;
+        uses patch as both a decorator and a context manager
+        """
+        payload = [
+                {"id": 328910, "name": "JavaWorks"},
+                {"id": "340103", "name": "ClibSync"}
+                ]
+        mock_get_json.return_value = payload
+        with patch(
+                "client.GithubOrgClient._public_repos_url"
+                ) as mock_public_repos_url:
+            mock_public_repos_url.return_value = "A repo url"
+
+            ghc = GithubOrgClient('org')
+            result = [load["name"] for load in payload]
+            self.assertEqual(ghc.public_repos(), result)
+            mock_get_json.assert_called_once()
+            mock_public_repos_url.called_with_once()
